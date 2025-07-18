b923e9a1420a28eec6e0eb6a6f970bf0813a6497
soon-hyeong <shjoo820@naver.com>
2025-07-18 17:56:45 +0900

    feat : mybatis product

---
+++ Spring15-MyBatis-product/src/main/java/org/kosa/myproject/MyBatisProductStudyRunner.java
@@ -0,0 +1,90 @@
+package org.kosa.myproject;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.kosa.myproject.domain.Product;
+import org.kosa.myproject.mapper.ProductMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBatisProductStudyRunner implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(MyBatisProductStudyRunner.class);
+	private final ProductMapper productMapper;
+
+	public MyBatisProductStudyRunner(ProductMapper productMapper) {
+		super();
+		this.productMapper = productMapper;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		int totalProductCount = productMapper.getTotalProductCount();
+//		logger.debug("총 상품수:{}", totalProductCount);
+		/*
+		 * 현 러너에서 DI : ProductMapper org.kosa.myproject.ProductMapper interface 를 정의한다
+		 * src/main/resources 아래에 동일한 패키지에
+		 */
+		// 전체 상품 조회
+//		List<Product> list = productMapper.findAllProductList();
+//		for(Product p: list)
+//			System.out.println(p);
+
+		// 상품 아이디로 개별 상품 조회
+		Long productId = 1L;
+//		Product product = productMapper.findProductById(productId);
+//		logger.debug("상품정보 {}", product);
+		
+//		// 상품 정보를 수정 : 상품명 productName 제조사 maker 가격 price
+//		Product param = new Product();
+//		param.setProductId(1L); // 상품 아이디가 1인 상품정보를 수정
+//		param.setProductName("갤럭시북");
+//		param.setMaker("삼성");
+//		param.setPrice(new BigDecimal("1500000"));
+//		int updateResult = productMapper.updateProduct(param);
+//		
+//		logger.debug("상품정보 수정 결과 {}", updateResult);
+//		
+//		Product product2 = productMapper.findProductById(productId);
+//		logger.debug("수정된 상품정보 확인 {}", product2);
+		
+		// 상품 등록 후 primary key인 product_id에 자동생성된 정보를 확보했는지 확인
+//		Product newProduct = new Product("갤럭시 26", "삼성", new BigDecimal("1400000"));
+//		int insertResult = productMapper.registerProduct(newProduct);
+//		logger.debug("등록 상품수{}", insertResult);
+//		logger.debug("총 상품수 {}", productMapper.getTotalProductCount());
+//		logger.debug("등록된 상품의 productId 정보 {}", newProduct.getProductId()); //null
+		
+		//가격대별 검색(낮은 가격 이상 ~ 높은 가격 이하)
+		BigDecimal minPrice = new BigDecimal("500000");
+		BigDecimal maxPrice = new BigDecimal("1000000");
+		List<Product> list = productMapper.findByPriceRange(minPrice, maxPrice);
+		for(Product p : list)
+			logger.debug("상품 정보 {}", p);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Spring15-MyBatis-product/src/main/java/org/kosa/myproject/domain/Product.java
@@ -0,0 +1,83 @@
+package org.kosa.myproject.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Product {
+	// primitiveDataType 은 null 값은 넣을 없으므로 Wrapper 클래스 타입을 사용한다
+	// 값이 존재하지 않을때는 null로 초기화됨
+	private Long productId;// table의 column : product_id BIGINT 
+	private String productName; // app은 camel case, db column은 underscore product_name
+	private String maker;
+	private BigDecimal price; //db decimal => 정확한 연산, 환율 등 금액 계산
+	private LocalDateTime createdAt;
+	
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Product(Long productId, String productName, String maker, BigDecimal price, LocalDateTime createdAt) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.maker = maker;
+		this.price = price;
+		this.createdAt = createdAt;
+	}
+
+	public Product(String productName, String maker, BigDecimal price) {
+		super();
+		this.productName = productName;
+		this.maker = maker;
+		this.price = price;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public LocalDateTime getcreatedAt() {
+		return createdAt;
+	}
+
+	public void setcreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", maker=" + maker + ", price="
+				+ price + ", createdAt=" + createdAt + "]";
+	}
+	
+	 
+}
